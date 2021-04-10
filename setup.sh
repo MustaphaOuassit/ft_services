6fae77661d4dd66c9e559755c0fa457fe37cef16
Mustapha Ouassit <mouassit@e1r3p2.1337.ma>
2021-04-10 14:03:35 +0100

    add phpmyadmin

--- setup.sh
+++ setup.sh
@@ -2,11 +2,13 @@ minikube delete
 minikube start --memory=2048
 eval $(minikube docker-env)
 minikube addons enable metallb
- kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+#kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 
 docker build -t nginx ./srcs/nginx
+docker build -t mysql ./srcs/mysql
 
 kubectl apply -f ./srcs/metalLB.yaml
 kubectl apply -f ./srcs/nginx/nginx.yaml
+kubectl apply -f ./srcs/mysql/mysql.yaml
 
 minikube dashboard
---
+++ srcs/mysql/start.sh
@@ -0,0 +1,13 @@
+rc-status
+touch /run/openrc/softlevel
+/etc/init.d/mariadb setup
+rc-service mariadb restart
+mysql -e "CREATE USER 'mustapha'@'%' IDENTIFIED BY 'mustapha';" 
+mysql -e "CREATE DATABASE wordpress;"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'mustapha'@'%';"
+mysql -e "FLUSH PRIVILEGES;"
+#mysql < /wordpress.sql
+mysql -e "CREATE DATABASE phpmyadmin;"
+#mysql < /phpmyadmin.sql
+rc-service mariadb restart
+sleep infinity
